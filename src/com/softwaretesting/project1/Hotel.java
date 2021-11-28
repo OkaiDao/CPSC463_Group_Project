896d31aceac91f77c74fbd99232a58f96bbe915f
Jonathan Dao <42830528+OkaiDao@users.noreply.github.com>
2021-11-27 18:33:32 -0800

    Update Hotel.java

--- src/com/softwaretesting/project1/Hotel.java
+++ src/com/softwaretesting/project1/Hotel.java
@@ -5,11 +5,64 @@ import java.util.ArrayList;
 class Hotel {
 
     private ArrayList<Room> rooms;
-
+    //Assumes that Type1 + 2 + 3 are < 100
+    private Hotel(int Type1, int Type2, int Type3)
+    {
+    	int roomIt = 0;
+    	RoomType roomType;
+    	RoomStatus roomStat;
+    	
+    	if (Type1 + Type2 + Type3 > 100)
+    	{
+    		System.out.println("Failed to create rooms list." + "\n");
+    		return;
+    	}
+    	
+    	while (roomIt < 100)
+    	{
+    		if(roomIt<Type1)
+    		{
+    			roomType = RoomType.KING;
+    			roomStat = RoomStatus.AVAILABLE;
+    		}
+    		else if(roomIt<(Type1+Type2))
+    		{
+    			roomType = RoomType.DOUBLE_QUEEN;
+    			roomStat = RoomStatus.AVAILABLE;
+    		}
+    		else if(roomIt<(Type1+Type2+Type3))
+    		{
+    			roomType = RoomType.DOUBLE_QUEEN_WITH_KITCHEN;
+    			roomStat = RoomStatus.AVAILABLE;
+    		}
+    		else
+    		{
+    			roomType = RoomType.SUITE;
+    			roomStat = RoomStatus.AVAILABLE;
+    		}
+    		Room tempRoom = new Room(roomType, roomStat, roomIt);
+    		rooms.add(tempRoom);
+    		//this.rooms[roomIt].Room(roomType, roomStat, roomIt); 
+    	}
+    }
+    
     class Room {
         private RoomType roomType;
         private RoomStatus roomStatus;
         private int roomNumber;
+        
+        public Room()
+        {
+        	this.roomType = RoomType.KING;
+        	this.roomStatus = roomStatus.UNAVAILABLE_DIRTY;
+        	this.roomNumber = 0;
+        }
+        public Room(RoomType roomType, RoomStatus roomStat, int roomNum)
+        {
+        	this.roomType = roomType;
+        	this.roomStatus = roomStat;
+        	this.roomNumber = roomNum;
+        }
     }
 
     class Reservation {
